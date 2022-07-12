0aaa819c5ead7e4d08364cfff0c6240367c92c47
lordyx1 <aalordy1709@gmail.com>
2022-07-12 13:20:21 +1000

    CSS update

--- src/App.jsx
+++ src/App.jsx
@@ -204,7 +204,7 @@ const memberList = useMemo(() => {
 if (hasClaimedNFT) {
   return (
     <div className="member-page">
-      <h1>🍪DAO Member Page</h1>
+      <h1>🔔DAO Member Page</h1>
       <p>Congratulations on being a member</p>
       <div>
         <div>
